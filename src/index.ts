23b2a65add5d5a9444828b24c308dad9f282354d
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-11-18 17:05:46 +0800

    chore: update deps

--- src/index.ts
+++ src/index.ts
@@ -51,8 +51,8 @@ export async function fetchAndExtractPackage(options: { name: string, dist?: str
 
     // Get the package tarball URL
     const tgzPath = await Promise.any([
-      downloadWithHttp(name, tempDir, tempFile, retry, logger),
-      downloadWithNpmHttp(name, tempDir, tempFile, retry, logger),
+      // downloadWithHttp(name, tempDir, tempFile, retry, logger),
+      // downloadWithNpmHttp(name, tempDir, tempFile, retry, logger),
       downloadWitchPack(name, tempDir, retry, logger),
     ])
 
@@ -117,7 +117,7 @@ async function retryAsync<T>(fn: () => Promise<T>, retries: number): Promise<T> 
 export async function downloadWitchPack(name: string, tempDir: string, retry: number, logger: any) {
   await retryAsync(() => {
     return new Promise((resolve, reject) => {
-      const { result, status } = jsShell(`npm pack ${name} --pack-destination ${tempDir}`, 'pipe')
+      const { result, status } = jsShell(`npm pack ${name} --pack-destination ${tempDir}`, 'ignore')
       if (status !== 0) {
         logger.error(result)
         reject(result)
@@ -135,7 +135,7 @@ export async function downloadWitchPack(name: string, tempDir: string, retry: nu
 export async function downloadWithNpmHttp(name: string, tempDir: string, tempFile: string, retry: number, logger: any) {
   const tarballUrl = await retryAsync(async () => {
     return new Promise((resolve, reject) => {
-      const { result, status } = jsShell(`npm view ${name} dist.tarball`, 'pipe')
+      const { result, status } = jsShell(`npm view ${name} dist.tarball`, 'ignore')
       if (status !== 0) {
         logger.error(result)
         reject(result)
